ae5ed6da0e11c7b2828b6ab687a54f6f34e3de59
jordi <jordimarset22@gmail.com>
2025-10-21 17:55:39 +0200

    actualizado

--- Funciones/Practica/script.js
+++ Funciones/Practica/script.js
@@ -1,6 +1,8 @@
 /* EJERCICIO 1 */
-/*
+
 function ejercicio1(){
+
+    //SOLICITAMOS AL USUARIO QUE INTRODUZCA PALABRAS HASAT QUE LO DEJE VACIO
     let resp = '';
     let respuestas = [];
     do{
@@ -11,26 +13,27 @@ function ejercicio1(){
             respuestas.push(resp);
         }
     }while(resp != '' || resp == null);
-
+    
     console.log(respuestas);
 
+    //CREAMOS UN SET CON LOS VALORES DEL ARRAY RESPUESTAS
     let valoresUnicos = new Set(respuestas)
 
     console.log(valoresUnicos);
 
+    //ORDENAMOS EL ARRAY
     valoresUnicosOrdenado = [...valoresUnicos];
 
     valoresUnicosOrdenado = valoresUnicosOrdenado.sort();
     console.log(valoresUnicosOrdenado);
 
-
+    //ORDENAMOS EL ARRAY DE FORMA REVERSA
     valoresUnicosReverse = valoresUnicosOrdenado.reverse();
 
     console.log(valoresUnicosReverse);
 }
 
-ejercicio1();
-*/
+
 
 
 
@@ -42,6 +45,7 @@ se deje vacío o se cancele.*/
 
 function ejercicio2(){
 
+    //SOLICITAMOS AL USUARIO QUE INTRODUZCA PALABRAS HASAT QUE LO DEJE VACIO
     let resp = '';
     let respuestas = [];
     do{
@@ -53,6 +57,29 @@ function ejercicio2(){
         }
     }while(resp);
 
+    console.log(respuestas);
+
+    //CREAMOS UN MAPA
+    let respuestasMap = new Map();
+
+    //RECORREMOS EL ARRAY RESPUESTA Y AÑADIMOS AL MAPA RESPUESTA A RESPUESTA
+    for(let respuesta of respuestas){
+        //COMPRUEBA SI LA PALABRA YA ESTA AGREGADA, SI ESTA AGREGADA SE LE SUMA UN 1, SI NO ESTÁ SE LE ASIGNA 0
+        respuestasMap.set(respuesta, (respuestasMap.get(respuesta) || 0) + 1)
+    }
+
+    console.log(respuestasMap);
 }
 
-ejercicio2();
+
+//CREAMOS EL ARRAY QUE POSTERIORMENTE LE PASAREMOS A LA FUNCIÓN
+let array = [1,2,3,4];
+
+function callback(array){
+    array = array.filter(function(num){
+        return num*2;
+    });
+    console.log(array);
+}
+
+callback(array);
